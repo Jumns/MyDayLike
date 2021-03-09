36781345643a1d893c3ad3379fdb9e65895f2671
Nathan Jonah <isjonathaniel@gmail.com>
2021-03-09 23:01:10 +0100

    added localstorage

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const secondContainer = document.querySelector("#container-2nd ul");
 if (container) {
   container.addEventListener("click", function (e) {
     if (e.target.className == "fa fa-check done-btn") {
-      if (confirm("Yes, I'm done with you")) {
-        // const li = e.target.parentElement.parentElement.parentElement;
-        // use target.closest when the parent element are different
+      if (confirm("Are you done with me?")) {
+        // use target.closest when the parent element is different
         const li = e.target.closest("li");
         container.removeChild(li);
 
@@ -20,15 +19,7 @@ if (container) {
           <span class="fa fa-undo done-btn" title="Undo"></span>
         `;
 
-        // option.innerHTML =
-        //   ' <div class="icons"> ' +
-        //   '<i class="far fa-clock"> ' +
-        //   time() +
-        //   "</i>" +
-        //   "</div>" +
-        //   '<span class="fa fa-undo done-btn" title="Undo"></span>';
-
-        // append removed todo to finished todos
+        // append removed todo to finished schedule
         secondContainer.append(li);
       }
     }
@@ -38,6 +29,7 @@ if (container) {
 // undo-btn //
 // const secondContainer = document.querySelector("#container-2nd ul");
 if (secondContainer) {
+  secondContainer.style.opacity = 0.5;
   secondContainer.addEventListener("click", function (e) {
     if (e.target.className == "fa fa-undo done-btn") {
       if (confirm("Sure to undo?")) {
@@ -56,7 +48,7 @@ if (secondContainer) {
           </div>
           <span class="fa fa-check done-btn" title="Undo"></span>
         `;
-        // append undo to schedule todos
+        // append undo to schedule queue
         container.append(li);
       }
     }
@@ -67,35 +59,32 @@ if (secondContainer) {
 if (container) {
   container.addEventListener("click", function (e) {
     if (e.target.className == "fa fa-trash") {
-      if (confirm("Sure to delete?")) {
+      if (confirm("want to delete me?")) {
         const li = e.target.closest("li");
         container.removeChild(li);
+        schedules.forEach((schedule) => {
+          localStorage.removeItem(schedule);
+        });
+        localStorage.removeItem(schedules.forEach(schedule));
       }
     }
   });
 }
 
-// add schedule  //
-const form = document.forms["add-schedule"];
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const value = form.querySelector("textarea").value;
-
-  // add new elements and classes
+const createSchedule = (value) => {
   const li = document.createElement("li");
 
   const taskCard = document.createElement("div");
-  taskCard.className = "task-card";
+  taskCard.classList.add("task-card");
 
   const schedule = document.createElement("p");
-  schedule.className = "schedule";
+  schedule.classList.add("schedule");
 
   const options = document.createElement("div");
-  options.className = "options";
+  options.classList.add("options");
 
   const optionIcons = document.createElement("div");
-  optionIcons.className = "icons";
+  optionIcons.classList.add("icons");
 
   const deleteIcon = document.createElement("i");
   deleteIcon.className = "fa fa-trash";
@@ -105,14 +94,15 @@ form.addEventListener("submit", function (e) {
   editIcon.className = "fa fa-edit";
   editIcon.setAttribute("title", "Edit");
 
-  const timeStamp = document.createElement("i");
+  const timeStamp = document.createElement("span");
   timeStamp.className = "far fa-clock";
 
   const doneBtn = document.createElement("span");
   doneBtn.className = "fa fa-check done-btn";
   doneBtn.setAttribute("title", "Done");
-  //add textnode
-  schedule.appendChild(document.createTextNode(value));
+  //add textNode
+  schedule.innerText = value;
+  saveLocalSchedule(value);
 
   // time stamp
   let date = new Date();
@@ -123,11 +113,9 @@ form.addEventListener("submit", function (e) {
 
   // append new elements
   if (value.trim() === null || value.trim() === "") {
-    alert("Please Enter a valid schedule.");
+    // alert("Please Enter a  schedule.");
     return false;
-  }
-
-  {
+  } else {
     taskCard.appendChild(schedule);
     taskCard.appendChild(options);
     options.appendChild(optionIcons);
@@ -140,47 +128,64 @@ form.addEventListener("submit", function (e) {
     li.append(taskCard);
     container.appendChild(li);
   }
+};
+
+// add schedule  //
+const form = document.querySelector("#add-schedule");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const value = form.querySelector("textarea").value;
+  createSchedule(value);
 
   // clearing value from textarea
   form.querySelector("textarea").value = "";
 });
 
 // hide schedules
-hideBox = document.querySelector("#hide");
-hideBox.addEventListener("change", function (e) {
-  if (hideBox.checked) {
-    container.style.display = "none";
-  } else {
-    container.style.display = "initial";
-  }
-});
+// hideBox = document.querySelector("#hide");
+// hideBox.addEventListener("change", function (e) {
+//   if (hideBox.checked) {
+//     container.style.display = "none";
+//   } else {
+//     container.style.display = "initial";
+//   }
+// });
 
 function time() {
-  const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let dn = "";
 
-  let h = hours;
-  let m = minutes;
-  let dn;
-
-  if (Number(hours) > 12) {
-    h = Number(hours) - 12;
-    if (Number(hours) < 10) {
-      h = "0" + h;
-    }
-    dn = "pm";
-  } else if (Number(hours) == 0) {
-    h = 12;
+  // checking for am or pm
+  if (hour < 12) {
     dn = "am";
-  } else if (Number(hours) == 12) {
-    dn = "pm";
   } else {
-    dn = "am";
+    dn = "pm";
   }
 
-  let formattedTime = h + ":" + m + " " + dn;
-  return formattedTime;
+  // convert time to 12 hours
+  if (hour == 0) {
+    hour = 12;
+  }
+
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+
+  hour = checkTime(hour);
+  minute = checkTime(minute);
+
+  function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+  let timeFormat = hour + ":" + minute + dn;
+  return timeFormat;
 }
 
 // edit todo
@@ -189,7 +194,7 @@ if (container) {
     if (e.target.className == "fa fa-edit") {
       const li = e.target.closest("li");
       const schedule = li.querySelector(".schedule");
-      let updatedTodo = prompt("Edit Todo", schedule.textContent);
+      let updatedTodo = prompt("Edit Schedule", schedule.textContent);
       if (updatedTodo) {
         schedule.textContent = updatedTodo;
       }
@@ -254,3 +259,85 @@ function displayTime() {
   printTime.textContent = time;
 }
 setInterval(displayTime, 1000);
+
+// LocalStorage function
+function saveLocalSchedule(schedule) {
+  // check if there's anything saved
+  let schedules;
+  if (localStorage.getItem("schedules")) {
+    schedules = JSON.parse(localStorage.getItem("schedules"));
+  } else {
+    schedules = [];
+  }
+
+  schedules.push(schedule);
+  localStorage.setItem("schedules", JSON.stringify(schedules));
+}
+
+document.addEventListener("DOMContentLoaded", getSchedules);
+
+// Get schedule from the localStorage
+function getSchedules() {
+  // check if there's anything saved
+  let schedules;
+  if (localStorage.getItem("schedules")) {
+    schedules = JSON.parse(localStorage.getItem("schedules"));
+  } else {
+    schedules = [];
+  }
+
+  schedules.forEach((scheduleItem) => {
+    const li = document.createElement("li");
+
+    const taskCard = document.createElement("div");
+    taskCard.classList.add("task-card");
+
+    const schedule = document.createElement("p");
+    schedule.classList.add("schedule");
+
+    const options = document.createElement("div");
+    options.classList.add("options");
+
+    const optionIcons = document.createElement("div");
+    optionIcons.classList.add("icons");
+
+    const deleteIcon = document.createElement("i");
+    deleteIcon.className = "fa fa-trash";
+    deleteIcon.setAttribute("title", "Delete");
+
+    const editIcon = document.createElement("i");
+    editIcon.className = "fa fa-edit";
+    editIcon.setAttribute("title", "Edit");
+
+    const timeStamp = document.createElement("span");
+    timeStamp.className = "far fa-clock";
+
+    const doneBtn = document.createElement("span");
+    doneBtn.className = "fa fa-check done-btn";
+    doneBtn.setAttribute("title", "Done");
+    //add textNode
+    schedule.innerText = scheduleItem;
+    // time stamp
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    timeStamp.appendChild(document.createTextNode(" " + time()));
+    if (scheduleItem.trim() === null || scheduleItem.trim() === "") {
+      // alert("Please Enter a  schedule.");
+      return false;
+    } else {
+      taskCard.appendChild(schedule);
+      taskCard.appendChild(options);
+      options.appendChild(optionIcons);
+      options.appendChild(doneBtn);
+      optionIcons.appendChild(deleteIcon);
+      optionIcons.appendChild(editIcon);
+      optionIcons.appendChild(timeStamp);
+
+      // appendChild to parent
+      li.append(taskCard);
+      container.appendChild(li);
+    }
+  });
+}
